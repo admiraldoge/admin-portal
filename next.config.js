d273499937db15145768846575147bb7ead6d0d8
admiraldoge <max.vinsum@gmail.com>
2022-01-05 06:38:06 -0400

    Fix build issue

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_PANAMA_HOST: process.env.NEXT_PUBLIC_PANAMA_HOST
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
   }
 }
