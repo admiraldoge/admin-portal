f4d3ac41098c842aa1d9924a3c0ac8e11cc30076
admiraldoge <max.vinsum@gmail.com>
2022-01-04 20:43:50 -0400

    Refactoring of lists

--- services/listService.tsx
+++ services/listService.tsx
@@ -3,9 +3,9 @@ import {setList, setTable} from "../redux/actions";
 import {ROLE, USER} from "../constants/subjects";
 import {CHART_ACCOUNT_LIST} from "../constants/lists";
 
-export const getListOfChartAccounts = () => async (dispatch:any) => {
+export const getList = (list:any) => async (dispatch:any) => {
 	const request
-		= await fetch(`${process.env.NEXT_PUBLIC_PANAMA_HOST}/chart-accounts?page=1&limit=999999999`,
+		= await fetch(`${process.env.NEXT_PUBLIC_PANAMA_HOST}${list.path}?page=1&limit=999999999`,
 		{
 			method: "GET",
 			credentials: 'include',
@@ -15,6 +15,6 @@ export const getListOfChartAccounts = () => async (dispatch:any) => {
 		});
 	const response = await request.json();
 	const data = response.data;
-	dispatch(setList({[CHART_ACCOUNT_LIST]: data.items}))
+	dispatch(setList({[list.name]: data.items}))
 	return data;
 }
