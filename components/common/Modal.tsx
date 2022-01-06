74c2726ff05c4309d63757e4028fb1b724108cee
admiraldoge <max.vinsum@gmail.com>
2022-01-06 03:09:23 -0400

    Fix modal styles

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {Modal as MuiModal} from '@mui/material';
+import {Modal as MuiModal, useMediaQuery} from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from "@mui/material/Grid";
 import CloseIcon from '@mui/icons-material/Close';
@@ -12,12 +12,15 @@ type actionsModalProps = {
 }
 
 const Modal: FC<actionsModalProps> = ({children, open, setOpen, width= 400}) => {
+
+	const isMobile = useMediaQuery('(max-width:600px)');
+	const responsiveWidth = isMobile ? 350 : (width ? width : 200);
 	const style = {
 		position: 'absolute' as 'absolute',
 		top: '50%',
 		left: '50%',
 		transform: 'translate(-50%, -50%)',
-		width: width,
+		width: responsiveWidth,
 		bgcolor: 'background.paper',
 		border: '2px solid #000',
 		boxShadow: 24,
